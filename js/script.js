89d3dad3c77b4b0b021704959aa3bcf6c68c707c
Christian Rufino <132108817+ChrisRufino@users.noreply.github.com>
2023-11-24 10:12:45 -0300

    npmNode

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 import { Router } from './router.js';
 
 const router = new Router();
-
+router.add('/', '/pages/home.html');
 router.add('/home', '/pages/home.html');
 router.add('/exploracao', '/pages/exploracao.html');
 router.add('/universe', '/pages/universe.html');
